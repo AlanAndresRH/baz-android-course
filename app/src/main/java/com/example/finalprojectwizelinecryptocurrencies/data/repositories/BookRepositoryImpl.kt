b4561423d68a27247bb8c5fd8c750ff0e35e730c
arosash <alan.rosash@elektra.com.mx>
2022-11-04 16:29:04 -0600

    Completion of Room implementation in the entire project and implementation of navigation controller

--- app/src/main/java/com/example/finalprojectwizelinecryptocurrencies/data/repositories/BookRepositoryImpl.kt
+++ app/src/main/java/com/example/finalprojectwizelinecryptocurrencies/data/repositories/BookRepositoryImpl.kt
@@ -19,7 +19,6 @@ class BookRepositoryImpl @Inject constructor(
     private val bookDao: BookDao,
     private val bookDetailDao: BookDetailDao,
     private val orderBookDao: OrderBookDao
-
 ) : CryptocurrencyRepository {
 
     override suspend fun getBooks(): Result<List<Book>> = withContext(Dispatchers.IO) {
